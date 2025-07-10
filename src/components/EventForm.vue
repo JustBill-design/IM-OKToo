4737673de7e84786000b6b2376100f88da74794b
JustBill <61508157+JustBill-design@users.noreply.github.com>
2025-07-10 12:50:02 +0800

    added event validation

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,20 +1,49 @@
 <script setup lang="ts">
-import EventTitle from './components/EventTitle.vue'
-import EventDateTimeRange from './components/EventDateTimeRange.vue'
-import EventGuest from './components/EventGuest.vue'
-import EventLocation from './components/EventLocation.vue'
-import EventDescription from './components/EventDescription.vue'
-import EventRecurrence from './components/EventRecurrence.vue'
+import EventTitle from '@/components/EventTitle.vue'
+import EventDateTimeRange from '@/components/EventDateTimeRange.vue'
+import EventGuest from '@/components/EventGuest.vue'
+import EventLocation from '@/components/EventLocation.vue'
+import EventDescription from '@/components/EventDescription.vue'
+import EventRecurrence from '@/components/EventRecurrence.vue'
 
 import { Button } from "@/components/ui/button"
 import { useForm } from 'vee-validate'
+import * as z from 'zod'
+import { toTypedSchema } from '@vee-validate/zod'
 
-const form = useForm()
+const d = new Date()
+let year = d.getFullYear();
+let month = d.getMonth();
 
-const onSubmit = form.handleSubmit((values) => {
-  console.log('Form submitted!', values)
+const formSchema = toTypedSchema(z.object({
+  title: z.string().min(1, 'Title is required'),
+  startDate: z.object({
+    era: z.literal("AD"),
+    year: z.coerce.number().int().min(year),
+    month: z.coerce.number().int(),
+    day: z.coerce.number().int(),
+  }).transform(({ year, month, day }) => new Date(year, month-1, day)),
+  startTime: z.string(),
+  endDate: z.object({
+    era: z.literal("AD"),
+    year: z.coerce.number().int().min(year),
+    month: z.coerce.number().int(),
+    day: z.coerce.number().int(),
+  }).transform(({ year, month, day }) => new Date(year, month-1, day)),
+  endTime: z.string(),
+  guests: z.string().optional(),
+  location: z.string().optional(),
+  description: z.string().optional(),
+  recurrence: z.string().optional(),
+}))
+
+const form = useForm({
+  validationSchema: formSchema,
 })
 
+const onSubmit = form.handleSubmit((values) => {
+  console.log('Form submitted:', values)
+})
 
 </script>
 
@@ -23,6 +52,7 @@ const onSubmit = form.handleSubmit((values) => {
         <h2 class="text-2xl font-bold mb-6 text-gray-800">Create Event</h2>
         <form @submit.prevent="onSubmit" class="space-y-6">
         <EventTitle/>
+        <pre>{{ form.values }}</pre>
         <EventDateTimeRange/>
         <EventGuest/>
         <EventLocation/>
