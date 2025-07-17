452bdf005729090db4dc1754cd301f84dcc4df72
JustBill <61508157+JustBill-design@users.noreply.github.com>
2025-07-17 23:32:13 +0800

    made it so that is working now
    - will try to fix why the forum section not working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,54 +1,9 @@
 <script setup lang="ts">
-import '@schedule-x/theme-shadcn/dist/index.css'
-import { ScheduleXCalendar } from '@schedule-x/vue'
-import { createCurrentTimePlugin } from '@schedule-x/current-time'
-import { createScrollControllerPlugin } from '@schedule-x/scroll-controller'
-import { Button } from "@/components/ui/button"
-import {
-  createCalendar,
-  createViewDay,
-  createViewMonthAgenda,
-  createViewMonthGrid,
-  createViewWeek,
-  createViewList
-} from '@schedule-x/calendar'
-import '@schedule-x/theme-default/dist/index.css'
-import SideBar from './components/SideBar/SideBar.vue'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
+import SideBar from './components/SideBar/SideBar.vue'
 
 const route = useRoute()
-
-const scrollController = createScrollControllerPlugin({
-  initialScroll: '07:50'
-})
-
-// Do not use a ref here, as the calendar instance is not reactive, and doing so might cause issues
-// For updating events, use the events service plugin
-const calendarApp = createCalendar({
-  views: [
-    createViewDay(),
-    createViewWeek(),
-    createViewList(),
-    createViewMonthGrid(),
-    createViewMonthAgenda(),
-  ],
-  events: [
-    {
-      id: 1,
-      title: 'Event 1',
-      start: '2023-12-19',
-      end: '2023-12-19',
-    },
-    {
-      id: 2,
-      title: 'Event 2',
-      start: '2023-12-20 12:00',
-      end: '2023-12-20 13:00',
-    },
-  ],
-  theme: 'shadcn'
-}, [createCurrentTimePlugin(), scrollController])
 
 // Track sidebar state for main content adjustment
 const sidebarCollapsed = ref(true)
