1875d30450590f34306cfdeead11fd1a3b332072
dora-ddw <1007968@mymail.sutd.edu.sg>
2025-07-25 11:26:14 +0800

    Placeholder for settings

---
+++ frontend/src/components/Settings.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="settings"></div>
+        <div class="settings-header"></div>
+            <h1 class="">Settings</h1>
+        
+        <div class="settings-content"></div>
+        
+
+
+</template>
+<script setup>
